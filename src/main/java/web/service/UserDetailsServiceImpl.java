b861f1fbb73c036f7077e888bf8a56573aa75312
StanislavPal <stasunado@gmail.com>
2020-11-22 21:06:07 +0400

    UserDetailsServiceImpl through userService & encodingFilter

--- src/main/java/web/service/UserDetailsServiceImpl.java
+++ src/main/java/web/service/UserDetailsServiceImpl.java
@@ -12,43 +12,47 @@ import web.model.User;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-/*    ver1 */
-    @Autowired
-    @Qualifier("userDaoImp")
-    private final Dao<User> userDao;
+///*    ver1 */
+//    @Autowired
+//    @Qualifier("userDaoImp")
+//    private final Dao<User> userDao;
+//
+//    public UserDetailsServiceImpl(Dao<User> userDao) {
+//        this.userDao = userDao;
+//    }
+//
+////     «Пользователь» – это просто Object. В большинстве случаев он может быть
+////      приведен к классу UserDetails.
+////     Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = userDao.findOne(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException( String.format("User '%s' not found!", username) );
+//        }
+//        return user;
+//    }
+///* end ver1 */
 
-    public UserDetailsServiceImpl(Dao<User> userDao) {
-        this.userDao = userDao;
+
+/*    ver2 */
+    private final UserService userService;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
     }
 
-//     «Пользователь» – это просто Object. В большинстве случаев он может быть
-//      приведен к классу UserDetails.
-//     Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
+    // «Пользователь» – это просто Object. В большинстве случаев он может быть
+    //  приведен к классу UserDetails.
+    // Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDao.findOne(username);
+        User user = userService.findOne(username);
         if (user == null) {
             throw new UsernameNotFoundException( String.format("User '%s' not found!", username) );
         }
         return user;
     }
-/* end ver1 */
-
-
-/////*    ver2 */
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserDetailsServiceImpl(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    // «Пользователь» – это просто Object. В большинстве случаев он может быть
-//    //  приведен к классу UserDetails.
-//    // Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return userService.findOne(username);
-//    }
-/////* end ver2 */
+/* end ver2 */
 }
